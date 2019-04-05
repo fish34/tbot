94bf862a59bc18cde39de8c9fcb62cc81eaffeb0
fish34 <fish34@mail.ru>
2019-04-05 21:05:22 +0300

    update urls

--- blog_telegram/urls.py
+++ blog_telegram/urls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf8 -*-
 
-from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^planet/', include('py_planet.urls', namespace='planet'))
+    path('^admin/', admin.site.urls),
+    path(r'^planet/', include('py_planet.urls', namespace='planet')),
 ]
